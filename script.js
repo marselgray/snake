f1c979ef056c59e0097c37a6961f6f958c79232a
Marsel Gray <marselgray@gmail.com>
2018-07-25 19:22:19 -0400

    movement feature

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var s;
 
+//sets up canvas and drawing ability 
 function setup(){
     createCanvas(600, 600)
     s = new Snake();
@@ -11,11 +12,33 @@ function draw(){
     s.show();
 }
 
+// key press functions from P5 Library 
+function keyPressed(){
+    if (keyCode === UP_ARROW){
+        s.dir(0, -1)
+    } else if (keyCode === DOWN_ARROW) {
+        s.dir(0, 1);
+    }
+    else if (keyCode === RIGHT_ARROW) {
+        s.dir(1, 0);
+    }
+    else if (keyCode === LEFT_ARROW) {
+        s.dir(-1, 0);
+    }
+}
+
+//actual snake (well not actual)
 function Snake(){
     this.x = 0;
     this.y = 0;
     this.xspeed = 1;
     this.yspeed = 0; 
+
+    //created function dir for keyPressed
+    this.dir = function(x, y){
+        this.xspeed = x;
+        this.yspeed = y;
+    }
 
     this.update = function(){
         this.x = this.x + this.xspeed;
